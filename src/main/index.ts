a614acd1d1a689de62feec160deaee221453f602
Douglas Brito <douglasmacb@Douglass-MBP.Home>
2022-01-29 12:07:29 +0000

    feat: add printer

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,11 +1,12 @@
 import { PrintDivisibleNumbers } from '../data/print-divisible-numbers'
 import { PrintFirstNumbers } from '../data/print-first-numbers'
 import { Printer } from '../domain/usecases/printer'
+import { NumbersUtils } from '../utils/numbers-utils'
 
 const printNumbers = (): void => {
   const numbers = [1, 2, 3, 4, 5, 6, 7]
 
-  const printers: Printer[] = [new PrintDivisibleNumbers(1, 100), new PrintFirstNumbers(numbers, 3)]
+  const printers: Printer[] = [new PrintDivisibleNumbers(NumbersUtils.ONE, NumbersUtils.HUNDRED), new PrintFirstNumbers(numbers, NumbersUtils.THREE)]
 
   for (const printer of printers) {
     printer.print().toConsole()
