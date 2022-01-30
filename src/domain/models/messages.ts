c5327392b643b3487382419a45cb60c714eb70c4
Douglas Brito <douglasmacb@Douglass-MBP.Home>
2022-01-30 09:13:26 +0000

    refactor: changed list array initialization

--- src/domain/models/messages.ts
+++ src/domain/models/messages.ts
@@ -1,10 +1,6 @@
 
 export class Messages {
-  private readonly list: Object[]
-
-  constructor () {
-    this.list = []
-  }
+  private readonly list: Object[] = []
 
   toConsole (): void {
     this.list.forEach(item => console.log(item))
